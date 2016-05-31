55028851044ca07734571bd106cbf1566973d663
tabtrader <developer@tab-trader.com>
2016-05-31 19:10:29 +0300

    Quoine: load pairs on init

--- xchange-quoine/src/test/java/org/knowm/xchange/quoine/dto/QuoineTest.java
+++ xchange-quoine/src/test/java/org/knowm/xchange/quoine/dto/QuoineTest.java
@@ -28,9 +28,6 @@ public class QuoineTest {
     ExchangeSpecification exSpec = new ExchangeSpecification(QuoineExchange.class);
 
     exSpec.setSslUri("https://api.quoine.com");
-    exSpec.setExchangeSpecificParametersItem(QuoineExchange.KEY_USER_ID, "137");
-
-    exSpec.setSecretKey("VB4iFYqW1WGxIjw8hWwqZzTZbgWuSy2Crw8bda/VoMz4IN6ovnCezlQbgoR42wJ1jGAK4G0dDtXuxlpezh1Bng==");
 
     Exchange exchangeAuth = ExchangeFactory.INSTANCE.createExchange(exSpec);
 
@@ -49,22 +46,9 @@ public class QuoineTest {
       UserTrades tradeHistory = pollingTradeService.getTradeHistory(new DefaultTradeHistoryParamCurrencyPair(CurrencyPair.BTC_USD));
       System.out.println(tradeHistory);
 
-
       tradeHistory = pollingTradeService.getTradeHistory(new DefaultTradeHistoryParamCurrencyPair(CurrencyPair.BTC_JPY));
       System.out.println(tradeHistory);
 
-//      LimitOrder limitOrder = new LimitOrder(Order.OrderType.ASK, new BigDecimal("0.01"), CurrencyPair.BTC_USD, null, new Date(), new BigDecimal("400"));
-//      String s = pollingTradeService.placeLimitOrder(limitOrder);
-//      System.out.println(s);
-//
-//
-//      OpenOrders openOrders = pollingTradeService.getOpenOrders();
-//      System.out.println(openOrders);
-//
-//      for (LimitOrder limitOrder : openOrders.getOpenOrders()) {
-//        pollingTradeService.cancelOrder(limitOrder.getId());
-//      }
-//
       OpenOrders openOrders = pollingTradeService.getOpenOrders();
       System.out.println(openOrders);
 
@@ -80,21 +64,6 @@ public class QuoineTest {
 
     Exchange exchange = ExchangeFactory.INSTANCE.createExchange(QuoineExchange.class.getName());
     PollingMarketDataService pollingMarketDataService = exchange.getPollingMarketDataService();
-
-/*
-    QuoineOrderType type1 = QuoineOrderType.market_with_range;
-    QuoineOrderType type2 = QuoineOrderType.margin_market_with_range;
-
-
-    QuoineOrderType type11 = QuoineOrderType.getOrderType(type1.getValue());
-    QuoineOrderType type22 = QuoineOrderType.getOrderType(type2.getValue());
-
-    System.out.println(type1 + " " + type1.getValue() + " " + type1.name());
-    System.out.println(type11 + " " + type11.getValue() + " " + type11.name());
-    System.out.println(type2 + " " + type2.getValue() + " " + type2.name());
-    System.out.println(type22 + " " + type22.getValue() + " " + type22.name());
-
-    System.out.println(QuoineOrderType.valueOf(type2.getValue()));*/
 
     Ticker ticker = pollingMarketDataService.getTicker(CurrencyPair.BTC_USD);
     System.out.println(ticker);
